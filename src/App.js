8b8aa9493e32dcf80862adcda9598005b521961e
DESKTOP-D1K3ME6\guilh <guilhermolv@hotmail.com>
2018-11-27 22:17:36 +0000

    Upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Layout, Header, Navigation, Drawer, ontent} from 'react-mdl';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+    <h1>Hello World</h1>
+</div>
+    );
+  }
+}
+
+export default App;
